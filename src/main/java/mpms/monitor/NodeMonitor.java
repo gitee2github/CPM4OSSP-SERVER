52f29fb38e76132d0ac47f59432f32913ad386b6
xyli <dev12102@linx-info.com>
2023-06-14 13:55:42 +0800

    创建NodeMonitor.java文件

---
+++ src/main/java/mpms/monitor/NodeMonitor.java
@@ -0,0 +1,28 @@
+package io.jpom.monitor;
+
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.cron.CronUtil;
+import cn.hutool.cron.pattern.CronPattern;
+import cn.hutool.cron.task.Task;
+import cn.jiangzeyin.common.JsonMessage;
+import cn.jiangzeyin.common.spring.SpringUtil;
+import com.alibaba.fastjson.JSONObject;
+import io.jpom.common.forward.NodeForward;
+import io.jpom.common.forward.NodeUrl;
+import io.jpom.model.Cycle;
+import io.jpom.model.data.NodeModel;
+import io.jpom.model.log.SystemMonitorLog;
+import io.jpom.service.dblog.DbSystemMonitorLogService;
+import io.jpom.service.node.NodeService;
+import io.jpom.util.CronUtils;
+
+import java.util.List;
+
+/**
+ * 节点监控
+ *
+ * 
+ 
+ */
+public class NodeMonitor implements Task {
+}
