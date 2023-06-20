eb3110bf83622a2db6f8284ba68e387389b3c92e
xyli <dev12102@linx-info.com>
2023-06-20 14:06:20 +0800

    创建NodeModel.java文件

---
+++ src/main/java/mpms/model/data/NodeModel.java
@@ -0,0 +1,18 @@
+package io.jpom.model.data;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.SecureUtil;
+import com.alibaba.fastjson.JSONArray;
+import io.jpom.common.forward.NodeUrl;
+import io.jpom.model.BaseModel;
+import io.jpom.model.Cycle;
+
+import java.util.Objects;
+
+/**
+ * 节点实体
+ *
+ */
+public class NodeModel extends BaseModel {
+
+}
